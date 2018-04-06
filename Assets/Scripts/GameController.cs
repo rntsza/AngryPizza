79255bd31351970e7f65ddae7a9ebbd4318f1867
Renato Gomes <reegomescel@gmail.com>
2018-04-06 17:25:29 -0300

    add gamecontroller
    ToDo

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Collections;
+
+public class GameController : Pizza {
+
+    float posX = 0;
+    float posYmin = 0;
+    float posYmax = 0;
+
+    public GameObject Inimigo;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Instantiate(Inimigo, new Vector3(posX = transform.position.x, posYmin = transform.position.y, posYmax = transform.position.z), tiros.transform.rotation);
+    }
+}
